46eee138212403df958632ebfb27aa78a5ff26a7
cauzinc <514322058@qq.com>
2019-07-21 20:18:38 +0800

    deal sync promises

--- utils/index.js
+++ utils/index.js
@@ -1,96 +1,114 @@
-const request = require('request')
-const fs = require('fs')
-const path = require('path')
-const cheerio = require('cheerio')
-const { recursionMkdir, IMAGE_DIR, VIDEO_DIR } = require('../config/paths')
-const httpConfig = require('../config/httpConfig')
-const streamDownload = require('./StreamDownload')
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const cheerio = require('cheerio');
+const { recursionMkdir, IMAGE_DIR, VIDEO_DIR } = require('../config/paths');
+const httpConfig = require('../config/httpConfig');
+const streamDownload = require('./StreamDownload');
 
 /**
- * get html body, return promise 
+ * get html body, return promise
  * @param base base url
  * @param index params xin the end of url
 */
 function getHtmlBodyByUrl(base, index) {
-    const url = typeof index === 'undefined' ? base : `${base}/${index}`
-    return new Promise((resolve, reject) => {
-        request(url, (error, res, body) => {
-            if (error) {
-                console.log('error in request', error)
-                resolve(false)
-            } else {
-                const $ = cheerio.load(body)
-                resolve($)
-            }
-        })
-    })
+  const url = typeof index === 'undefined' ? base : `${base}/${index}`;
+  return new Promise((resolve) => {
+    request(url, (error, res, body) => {
+      if (error) {
+        console.log('error in request', error);
+        resolve(false);
+      } else {
+        const $ = cheerio.load(body);
+        resolve($);
+      }
+    });
+  });
 }
 
-/** 
+/**
  * download image with Referer header info, return promise
- * @param imgUrl 
+ * @param imgUrl
  * @param dir './comic_1'
  * @param filename '1.jpg'
  * @param referer
 */
 async function downloadImageWithRefer(imgUrl, dir, filename, referer) {
-    let fileDir = path.resolve(IMAGE_DIR, dir)
-    let result = await recursionMkdir(fileDir)
-    let writeStream = fs.createWriteStream(`${fileDir}/${filename}`)
+  const fileDir = path.resolve(IMAGE_DIR, dir);
+  const result = await recursionMkdir(fileDir);
+  const writeStream = fs.createWriteStream(`${fileDir}/${filename}`);
 
-    return new Promise((resolve, reject) => {
-        if (!referer) {
-            return resolve('need referer !')
-        }
-        if (!result) {
-            return resolve('make dir error!')
-        }
-        let req = request({
-            url: imgUrl,
-            method: 'GET',
-            headers: {
-                'Referer': referer,
-                ...httpConfig.httpHeaders
-            }
-        })
-        req.pipe(writeStream)
-        req.on('end', (res) => {
-            console.log('download success', imgUrl)
-            resolve(true)
-        })
-        
-    })
+  return new Promise((resolve) => {
+    if (!referer) {
+      return resolve('need referer !');
+    }
+    if (!result) {
+      return resolve('make dir error!');
+    }
+    const req = request({
+      url: imgUrl,
+      method: 'GET',
+      headers: {
+        Referer: referer,
+        ...httpConfig.httpHeaders,
+      },
+    });
+    req.pipe(writeStream);
+    req.on('end', () => {
+      console.log('download success', imgUrl);
+      resolve(true);
+    });
+  });
 }
 
 async function downloadVideoWithRefer(videoUrl, dir, filename, referer) {
-    let fileDir = path.resolve(VIDEO_DIR, dir)
-    let result = await recursionMkdir(fileDir)
-    return new Promise((resolve, reject) => {
-        if (!referer) {
-            return resolve('need referer !')
-        }
-        if (!result) {
-            return resolve('make dir error!')
-        }
-        streamDownload.downloadFile({
-           filename,
-           fileDir
-        }, {
-            fileUrl: videoUrl,
-            headers: {
-                'Referer': referer,
-                ...httpConfig.httpHeaders
-            }
-        }, (status, percentage) => {
-            console.log(`${status}: ${percentage}`)
-        })
-        resolve(true)
-    })
+  const fileDir = path.resolve(VIDEO_DIR, dir);
+  const result = await recursionMkdir(fileDir);
+  return new Promise((resolve) => {
+    if (!referer) {
+      return resolve('need referer !');
+    }
+    if (!result) {
+      return resolve('make dir error!');
+    }
+    streamDownload.downloadFile({
+      filename,
+      fileDir,
+    }, {
+      fileUrl: videoUrl,
+      headers: {
+        Referer: referer,
+        ...httpConfig.httpHeaders,
+      },
+    }, (status, percentage) => {
+      console.log(`${status}: ${percentage}`);
+    });
+    resolve(true);
+  });
 }
 
-module.exports =  {
-    getHtmlBodyByUrl,
-    downloadImageWithRefer,
-    downloadVideoWithRefer
+/**
+ * @param {funcArr} funcArr array of function that returns a promise
+ * reduce 的第二个参数为默认值，第一次执行时取Promise.resolve()第一个prev
+ * next() 返回一个promise对象，作为下一次执行的prev
+ */
+async function executeSyncPromises(funcArr) {
+  return new Promise((resolve) => {
+    funcArr.reduce((prev, next, index) => {
+      if (index === funcArr.length - 1) {
+        return prev.then(() => {
+          next(resolve);
+        });
+      }
+      return prev.then(() => next());
+    }, Promise.resolve());
+    // resolve(true);
+  });
 }
 
+module.exports = {
+  getHtmlBodyByUrl,
+  downloadImageWithRefer,
+  downloadVideoWithRefer,
+  executeSyncPromises
+};
