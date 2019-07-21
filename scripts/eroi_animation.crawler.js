1003fb5862af4f7650b3c4e66045d9e1d74118d3
cauzinc <514322058@qq.com>
2019-07-21 10:29:11 +0800

    clear code

--- scripts/eroi_animation.crawler.js
+++ scripts/eroi_animation.crawler.js
@@ -1,6 +1,3 @@
-const request = require('request')
-const fs = require('fs')
-const cheerio = require('cheerio')
 const { downloadVideoWithRefer, getHtmlBodyByUrl } = require('../utils')
 
 async function download(index, filename) {
@@ -20,12 +17,5 @@ async function download(index, filename) {
         `${baseUrl}/${index}`
     )
 }
-
-// downloadVideoWithRefer(
-//     'https://zozovideo.com/wp-content/uploads/2019/06/%E6%A1%9C%E5%AE%AE%E5%A7%89%E5%A6%B9%E3%81%AE%E3%83%8D%E3%83%88%E3%83%A9%E3%83%AC%E8%A8%98%E9%8C%B2-1.mp4',
-//     './',
-//     'test.mp4',
-//     'http://xn--cckl0itdpc9763ahlyc.tv/archives/2991'
-// )
 
 download(380, `蠱惑の刻--腹ボテクール・雪乃～恥蜜に溢れる緩んだ恥穴.mp4`)
